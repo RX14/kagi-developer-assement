c56da4373aa3b0c36cacde19e4e74b68115bbea4
Stephanie Wilde-Hobbs <git@stephanie.is>
2021-03-08 16:18:59 +0000

    Add fuzzy Date and replace Range of time in extractors

--- spec/date_extraction_spec.cr
+++ spec/date_extraction_spec.cr
@@ -1,41 +1,53 @@
 require "./spec_helper"
 
 private alias DateExtraction = SearchEngine::DateExtraction
+private alias Date = DateExtraction::Date
 private alias Result = DateExtraction::Result
 
 describe SearchEngine::DateExtraction do
   describe "Result" do
     it "accepts confidence in range 0..10" do
-      now = Time.utc
-      Result.new(now..now, 0)
-      Result.new(now..now, 10)
+      date = Date.new(2021, 1, 1)
+      Result.new(date, 0)
+      Result.new(date, 10)
       expect_raises(ArgumentError, "confidence must be 0..10 (was -1)") do
-        Result.new(now..now, -1)
+        Result.new(date, -1)
       end
       expect_raises(ArgumentError, "confidence must be 0..10 (was 11)") do
-        Result.new(now..now, 11)
+        Result.new(date, 11)
       end
     end
 
     describe "exact?" do
-      it "returns true if start and end are on the same day" do
-        result = Result.new(Time.utc(2020, 1, 1, 0, 0, 0)..Time.utc(2020, 1, 1, 0, 0, 0), 0)
-        result.exact?.should be_true
-
-        result = Result.new(Time.utc(2020, 1, 1, 0, 0, 0)..Time.utc(2020, 1, 1, 23, 59, 59), 0)
-        result.exact?.should be_true
-
-        result = Result.new(Time.utc(2020, 1, 1, 23, 59, 59)..Time.utc(2020, 1, 1, 0, 0, 0), 0)
+      it "returns true if date is accurate to the day" do
+        result = Result.new(Date.new(2020, 1, 1), 0)
         result.exact?.should be_true
       end
 
-      it "returns false if start and end are on different days" do
-        result = Result.new(Time.utc(2020, 1, 1, 0, 0, 0)..Time.utc(2019, 12, 31, 23, 59, 59), 0)
-        result.exact?.should be_false
-
-        result = Result.new(Time.utc(2020, 1, 1, 0, 0, 0)..Time.utc(2020, 1, 2, 0, 0, 0), 0)
+      it "returns false if date is not accurate to one day" do
+        result = Result.new(Date.new(2020, 1), 0)
         result.exact?.should be_false
       end
+    end
+  end
+
+  describe ".extract_date" do
+    it "extracts a date from an exact URL" do
+      page = page("http://example.com/blog/2020/03/12/article", empty_html)
+      result = DateExtraction.extract_date(page)
+      result.should eq(Date.new(2020, 3, 12))
+    end
+
+    it "extracts a date from a non-exact URL" do
+      page = page("http://example.com/blog/2020/article", empty_html)
+      result = DateExtraction.extract_date(page)
+      result.should eq(Date.new(2020))
+    end
+
+    it "returns nil if no date can be extracted" do
+      page = page("http://example.com/blog/article", empty_html)
+      result = DateExtraction.extract_date(page)
+      result.should be_nil
     end
   end
 
@@ -45,36 +57,37 @@ describe SearchEngine::DateExtraction do
       result = DateExtraction.extract_date_from_url(page).not_nil!
 
       result.confidence.should eq(2)
-      result.range.should eq(Time.utc(2020, 3, 12)..Time.utc(2020, 3, 12).at_end_of_day)
-      result.exact?.should be_true
+      result.date.should eq(Date.new(2020, 3, 12))
 
       page = page("http://example.com/blog/2020/3/article", empty_html)
       result = DateExtraction.extract_date_from_url(page).not_nil!
 
       result.confidence.should eq(2)
-      result.range.should eq(Time.utc(2020, 3, 1)..Time.utc(2020, 3, 31).at_end_of_day)
-      result.exact?.should be_false
+      result.date.should eq(Date.new(2020, 3))
 
       page = page("http://example.com/blog/2020/article", empty_html)
       result = DateExtraction.extract_date_from_url(page).not_nil!
 
       result.confidence.should eq(2)
-      result.range.should eq(Time.utc(2020, 1, 1)..Time.utc(2020, 12, 31).at_end_of_day)
-      result.exact?.should be_false
+      result.date.should eq(Date.new(2020))
 
       page = page("http://example.com/blog/article-2020-2-3", empty_html)
       result = DateExtraction.extract_date_from_url(page).not_nil!
 
       result.confidence.should eq(2)
-      result.range.should eq(Time.utc(2020, 2, 3)..Time.utc(2020, 2, 3).at_end_of_day)
-      result.exact?.should be_true
+      result.date.should eq(Date.new(2020, 2, 3))
 
       page = page("http://example.com/blog/2020/2_3-article", empty_html)
       result = DateExtraction.extract_date_from_url(page).not_nil!
 
       result.confidence.should eq(2)
-      result.range.should eq(Time.utc(2020, 2, 3)..Time.utc(2020, 2, 3).at_end_of_day)
-      result.exact?.should be_true
+      result.date.should eq(Date.new(2020, 2, 3))
+
+      page = page("http://example.com/20210308-article", empty_html)
+      result = DateExtraction.extract_date_from_url(page).not_nil!
+
+      result.confidence.should eq(2)
+      result.date.should eq(Date.new(2021, 3, 8))
     end
 
     it "doesn't extract dates from dubious URLs" do
@@ -83,6 +96,15 @@ describe SearchEngine::DateExtraction do
       result.should be_nil
 
       page = page("http://example.com/21-2-8-article", empty_html)
+      result = DateExtraction.extract_date_from_url(page)
+      result.should be_nil
+
+      # 20210308 as part of another number, not on it's own
+      page = page("http://example.com/202103081-article", empty_html)
+      result = DateExtraction.extract_date_from_url(page)
+      result.should be_nil
+
+      page = page("http://example.com/120210308-article", empty_html)
       result = DateExtraction.extract_date_from_url(page)
       result.should be_nil
     end
--- src/date_extraction.cr
+++ src/date_extraction.cr
@@ -1,12 +1,17 @@
 require "./crawler"
 
-# Implements date extraction from `Page` objects.
+# Implements date extraction from `Page` objects using the HTML content and the
+# URL.
 module SearchEngine::DateExtraction
   DATE_EXTRACTORS = [
-    ->extract_date_from_url(Page),
-  ] of Page -> DateExtraction::Result?
+    ->extract_date_from_url(Crawler::Page),
+  ] of Crawler::Page -> DateExtraction::Result?
 
-  def self.extract_date(page : Page) : Time
+  # Extracts a date from a `Crawler::Page` by running all registered date
+  # extractors (in `DATE_EXTRACTORS`), and picking the highest-confidence result
+  # which is accurate to the day. If there is no result accurate to the day
+  # available, it'll pick the highest confidence fuzzy result.
+  def self.extract_date(page : Crawler::Page) : Date?
     results = DATE_EXTRACTORS.compact_map do |extractor|
       extractor.call(page)
     rescue ex
@@ -17,55 +22,64 @@ module SearchEngine::DateExtraction
     results.sort_by! { |r| -r.confidence }
 
     exact_result = results.find(&.exact?)
-    return exact_result.range.begin if exact_result
+    return exact_result.date if exact_result
 
-    # There are no exact results, choose the first
-    results.first.range.begin
+    # There are no exact results, choose the first non-exact result.
+    if result = results.first?
+      result.date
+    end
   end
 
-  class Result
-    # The range of times which
-    getter range : Range(Time, Time)
+  # Represents a fuzzy date in time. This is accurate to the year, month, or day
+  # depending on which fields are populated.
+  class Date
+    getter year : Int32
+    getter month : Int32?
+    getter day : Int32?
 
-    # Confidence (0..10)
+    def initialize(@year, @month = nil, @day = nil)
+      raise ArgumentError.new("Date has day but no month!") if @day && !@month
+    end
+
+    # Two dates are equal if their year, month, and day are identical
+    def_equals_and_hash @year, @month, @day
+  end
+
+  # Represents the result of a date extractor. Each date extractor will produce
+  # a `Date`, with associated confidence value betwene 0 and 10.
+  class Result
+    # The date extracted from the page.
+    getter date : Date
+
+    # The confidence of `date` (0..10).
     getter confidence : Int32
 
-    def initialize(@range, @confidence)
+    def initialize(@date, @confidence)
       unless 0 <= confidence <= 10
         raise ArgumentError.new("confidence must be 0..10 (was #{confidence})")
       end
     end
 
-    # Returns true if the start and end of range of this result are on the same
-    # day.
-    def exact?
-      range.begin.at_beginning_of_day == range.end.at_beginning_of_day
+    # Returns true if the date of this result is accurate to a single day.
+    def exact? : Bool
+      !!date.day
     end
   end
 
+  # Extracts a date from the URL of a page. Works for many blog pages which have
+  # URLs like "/blog/2020/01/2/title".
   def self.extract_date_from_url(page : Crawler::Page)
     # Match URLs which contain 2021-01-01 or 2021/01/01 or 20210101.
     # Pages before 2000 are not matched for false positives.
     # TODO: try and detect american date formats?
-    return unless match = page.url.path.match(/\b(20\d\d)[\/\-_]?(\d\d?)?[\/\-_]?(\d\d?)?\b/)
+    match = page.url.path.match(/\b(20\d\d)(?:[\/\-_](\d\d?)(?:[\/\-_](\d\d?))?)?\b/)
+    match ||= page.url.path.match(/\b(20\d\d)(\d\d)(\d\d)\b/)
+    return unless match
+
     year = match[1].to_i
     month = match[2]?.try(&.to_i)
     day = match[3]?.try(&.to_i)
 
-    case {year, month, day}
-    when {Int32, nil, nil}
-      time = Time.utc(year, 1, 1)
-      range = time..time.at_end_of_year
-    when {Int32, Int32, nil}
-      time = Time.utc(year, month, 1)
-      range = time..time.at_end_of_month
-    when {Int32, Int32, Int32}
-      time = Time.utc(year, month, day)
-      range = time..time.at_end_of_day
-    else
-      raise "BUG: unreachable"
-    end
-
-    Result.new(range, 2)
+    Result.new(Date.new(year, month, day), 2)
   end
 end
